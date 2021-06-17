2416bf1ae90491408c951374269a2c96788c9965
Rupsa <rupsadhar22@gmail.com>
2021-06-17 10:31:36 +0530

    routes addes

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 //connect db
 connectDB();
 
+//define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 app.get('/', (req,res) => res.send('API Running'));
 
 const PORT = process.env.PORT || 5000;
