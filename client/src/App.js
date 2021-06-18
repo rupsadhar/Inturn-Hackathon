bd70a9fd12a8ad046147ae21f9078ae1eb0f6ab0
Rupsa <rupsadhar22@gmail.com>
2021-06-18 10:27:08 +0530

    reg and login done

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,49 @@
 
 import './App.css';
 import React,{ Fragment} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import { useEffect } from 'react';
+
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+import { LOGOUT } from './actions/types';
 
 const App = () => {
+  useEffect(() => {
+    // check for token in LS
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+    store.dispatch(loadUser());
+
+    // log user out from all tabs if they log out in one tab
+    window.addEventListener('storage', () => {
+      if (!localStorage.token) store.dispatch({ type: LOGOUT });
+    });
+  }, []);
   return(
+    <Provider store={store}>
+      <Router>
         <Fragment>
           <Navbar />
-          <Landing />
-        </Fragment>
+          <Route exact path='/' component={Landing}/ >
+          <section className="container">
+            <Switch>
+            <Route exact path='/register' component={Register}/ >
+            <Route exact path='/login' component={Login}/ >
+
+            </Switch>
+          </section>
+      </Fragment>
+      </Router>
+      </Provider>
+  
   )
 };
 
